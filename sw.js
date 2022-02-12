5d778a8d1d0ecaaf8af345fa5bc914f101b5f18f
tsharris <tsharris@noctrl.edu>
2022-02-12 14:23:56 -0600

    c

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", e => {
-    console.log("Install dcode!")
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./src/master.css","./images/CDlogo_192x192.png"])
+        })
+    );
 });
